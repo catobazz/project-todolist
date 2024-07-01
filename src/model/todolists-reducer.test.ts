5659694a01fe25e61093c385b5289c1840d94366
Artem Rybin <catobazzz@gmail.com>
2024-07-01 16:06:19 +0300

    sprint-2-4 refactor: improve unit tests

--- src/model/todolists-reducer.test.ts
+++ src/model/todolists-reducer.test.ts
@@ -1,4 +1,10 @@
-import { todolistsReducer } from './todolists-reducer'
+import {
+  addTodolistAC,
+  changeTodolistFilterAC,
+  changeTodolistTitleAC,
+  removeTodolistAC,
+  todolistsReducer,
+} from './todolists-reducer'
 import { v1 } from 'uuid'
 import { TodolistType } from '../App'
 
@@ -13,13 +19,7 @@ test('correct todolist should be removed', () => {
   ]
 
   // 2. Действие
-  const action = {
-    type: 'REMOVE-TODOLIST',
-    payload: {
-      id: todolistId1,
-    },
-  } as const
-  const endState = todolistsReducer(startState, action)
+  const endState = todolistsReducer(startState, removeTodolistAC(todolistId1))
 
   // 3. Проверяем, что наши действия (изменения state) соответствуют ожиданию
   // в массиве останется один тудулист
@@ -37,16 +37,10 @@ test('correct todolist should be added', () => {
     { id: todolistId2, title: 'What to buy', filter: 'all' },
   ]
 
-  const action = {
-    type: 'ADD-TODOLIST',
-    payload: {
-      title: 'New Todolist',
-    },
-  } as const
-  const endState = todolistsReducer(startState, action)
+  const endState = todolistsReducer(startState, addTodolistAC('New Todolist'))
 
   expect(endState.length).toBe(3)
-  expect(endState[2].title).toBe(action.payload.title)
+  expect(endState[2].title).toBe('New Todolist')
 })
 
 test('correct todolist should change its name', () => {
@@ -58,17 +52,10 @@ test('correct todolist should change its name', () => {
     { id: todolistId2, title: 'What to buy', filter: 'all' },
   ]
 
-  const action = {
-    type: 'CHANGE-TODOLIST-TITLE',
-    payload: {
-      id: todolistId2,
-      title: 'New Todolist',
-    },
-  } as const
-  const endState = todolistsReducer(startState, action)
+  const endState = todolistsReducer(startState, changeTodolistTitleAC(todolistId2, 'New Todolist'))
 
   expect(endState[0].title).toBe('What to learn')
-  expect(endState[1].title).toBe(action.payload.title)
+  expect(endState[1].title).toBe('New Todolist')
 })
 
 test('correct filter of todolist should be changed', () => {
@@ -80,15 +67,8 @@ test('correct filter of todolist should be changed', () => {
     { id: todolistId2, title: 'What to buy', filter: 'all' },
   ]
 
-  const action = {
-    type: 'CHANGE-TODOLIST-FILTER',
-    payload: {
-      id: todolistId2,
-      filter: 'completed',
-    },
-  } as const
-  const endState = todolistsReducer(startState, action)
+  const endState = todolistsReducer(startState, changeTodolistFilterAC(todolistId2, 'completed'))
 
   expect(endState[0].filter).toBe('all')
-  expect(endState[1].filter).toBe(action.payload.filter)
+  expect(endState[1].filter).toBe('completed')
 })
